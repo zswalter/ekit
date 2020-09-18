f79edc4804ad122852d1d9f69c452e6c464d0e76
Zachary Denny <zdenny61@outlookcom>
2020-09-18 19:53:37 -0400

    Added logout menu item to main menu

--- EKit_Android/app/src/main/java/com/oakland/ekit/UserHomepageActivity.java
+++ EKit_Android/app/src/main/java/com/oakland/ekit/UserHomepageActivity.java
@@ -2,6 +2,8 @@ package com.oakland.ekit;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,6 +33,37 @@ public class UserHomepageActivity extends AppCompatActivity {
 
 
 
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        //add the menu to the view
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //check which menu item was pressed
+        if(id == android.R.id.home){
+
+            //TODO: if user presses the home button maybe do something??
+
+        }
+        else if(id == R.id.action_logout){
+            //TODO: call to logout user
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
